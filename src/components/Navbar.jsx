fe641867816d3b4644753eade412b125584b6cf8
nocturnalnerds <133097737+nocturnalnerds@users.noreply.github.com>
2024-08-18 02:28:49 +0700

    asset

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,47 @@
+import React, { useEffect } from "react";
+import logo from '../assets/'
+
+const Navbar = () => {
+    count[iseMenuOpen, setIsMenuOpen] = useState(false);
+    count[isSticky, setIsSticky] = useState(false);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            if (window.scrollY > 100) {
+                setIsSticky(true);
+                setIsMenuOpen(false)
+            }
+            else {
+                setIsSticky(false);
+            }
+        };
+
+        window.addEventListener("scroll", handleScroll);
+
+        return () => {
+            window.removeEventListener("scroll", handleScroll);
+        };
+    }, []);
+    const toggleMenu = () =>{
+        setIsMenuOpen(!iseMenuOpen);
+    }
+    const navItems = [
+        {link: "Home", path: "home" },
+        {link: "Service", path: "service" },
+        {link: "About", path: "about" },
+        {link: "Product", path: "product" },
+        {link: "Testimonial", path: "testimonial" },
+        {link: "FAQ", path: "faq" },
+    ];
+    return (
+        <header className="w-full bg-white md:bg-transparent fixed top-0 left-0 right-0">
+            <nav>
+                <div>
+                    <a href=""></a>
+                </div>
+            </nav>
+        </header>
+    );
+}
+
+export default Navbar;
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,15 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        'neutralSilver': '#F5F7FA',
+        'neutralDGrey': '#4D4D4D',
+        'brandPrimary': '#4CAF4F',
+        'neutralSilver': '#F5F7FA',
+        'neutralGrey': '##717171',
+      }
+    },
   },
   plugins: [],
 }
