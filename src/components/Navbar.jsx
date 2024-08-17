bf8447f6770d6adcdff18dfa1338c9eeffca2eba
nocturnalnerds <133097737+nocturnalnerds@users.noreply.github.com>
2024-08-18 02:30:10 +0700

    .

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import logo from '../assets/'
+import logo from '../assets/logo.png';
 
 const Navbar = () => {
     count[iseMenuOpen, setIsMenuOpen] = useState(false);
@@ -37,7 +37,7 @@ const Navbar = () => {
         <header className="w-full bg-white md:bg-transparent fixed top-0 left-0 right-0">
             <nav>
                 <div>
-                    <a href=""></a>
+                    <a href=""><img src={logo} alt="" />NEXCENT</a>
                 </div>
             </nav>
         </header>
